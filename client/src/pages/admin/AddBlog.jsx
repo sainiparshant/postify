6d73393a90bb2394966b83377677040629b7ffb7
parshantsaini <sainiparshant1901@gmail.com>
2025-11-17 09:24:07 +0530

    Add Quill editor for blog creation and enhance comments management UI

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { assets, blogCategories } from "../../assets/assets";
+import Quill from "quill";
 
 const AddBlog = () => {
-  return (
-    <div>AddBlog</div>
-  )
-}
 
-export default AddBlog
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
+  const [category, setCategory] = useState("startUp");
+  const [isPublished, setIsPublished] = useState(false);
+
+  const generateContent = async() =>{
+
+  }
+
+  const submitHanlder = async(e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+
+    if(!quillRef.current && editorRef.current){
+      quillRef.current = new Quill(editorRef.current, {theme: 'snow'})
+    }
+  }, []);
+
+  return (
+    <form
+      onSubmit={submitHanlder}
+      className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll"
+    >
+      <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
+        <p>Upload thumbnail</p>
+        <label htmlFor="image">
+          <img
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            alt=""
+            className="mt-2 h-16 rounded cursor-pointer"
+          />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
+        </label>
+
+        <p className="mt-4">Blog Title</p>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          type="text"
+          placeholder="Type here"
+          required
+          className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
+        />
+        <p className="mt-4">Sub Title</p>
+        <input
+          onChange={(e) => setSubTitle(e.target.value)}
+          value={subTitle}
+          type="text"
+          placeholder="Type here"
+          required
+          className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
+        />
+
+         <p className="mt-4">Blog description</p>
+         <div className="max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative">
+          <div ref={editorRef}></div>
+          <button type="button" onClick={generateContent} className="absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer">Generate with AI</button>
+         </div>
+
+         <p className="mt-4">Blog description</p>
+          <select 
+          onChange={(e) => setCategory(e.target.value)}
+          name="category" className="mt-2  px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded">
+            <option value="">Select Category</option>
+            {blogCategories.map((item,index) =>(
+              <option value={item} key={index}>{item}</option>
+            ))}
+
+          </select>
+
+          <div className="flex gap-2 mt-4">
+            <p>Publish Now</p>
+            <input 
+            onChange={(e) => setIsPublished(e.target.checked)}
+            type="checkbox" checked={isPublished} className="scale-125 cursor-pointer "/>
+          </div>
+
+          <button type="submit" className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm">Add Blog</button>
+        
+      </div>
+    </form>
+  );
+};
+
+export default AddBlog;
