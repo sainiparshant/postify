c3844564ae88a7fa327c4e209c581683120b0136
parshantsaini <sainiparshant1901@gmail.com>
2025-11-18 16:08:09 +0530

    gemini integrate

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { imagekit } from '../config/imageKit.js';
 import Blog from '../models/blog.model.js';
 import Comment from '../models/comment.model.js';
+import main from '../config/gemini.js'
 
 export const addBlog = async(req, res) =>{
     try {
@@ -169,6 +170,22 @@ export const getBlogComments = async (req,res) =>{
             comments
         });
 
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export const generateContent = async(req,res) =>{
+    try {
+        const {prompt} = req.body;
+        const content = await main(prompt + 'Generate a blog content for this topic in simple text format');
+        return res.json({
+            success: true,
+            content
+        });
     } catch (error) {
         return res.json({
             success: false,
--- server/routes/blog.routes.js
+++ server/routes/blog.routes.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { addBlog, addComment, deleteBlogById, getAllBlogs, getBlogById, getBlogComments, togglePublish } from '../controllers/blog.controller.js';
+import { addBlog, addComment, deleteBlogById, generateContent, getAllBlogs, getBlogById, getBlogComments, togglePublish } from '../controllers/blog.controller.js';
 import upload from '../middlewares/multer.js';
 import auth from '../middlewares/auth.js';
 
@@ -13,6 +13,9 @@ blogRouter.post("/delete",auth, deleteBlogById);
 blogRouter.post("/toggle-publish", auth, togglePublish);
 blogRouter.post("/add-comment" , addComment);
 blogRouter.post("/comments" , getBlogComments);
+
+blogRouter.post("/generate" , auth, generateContent);
+
 
 
 
